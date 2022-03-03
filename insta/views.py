87c9c9cfcb153e1af6b4d10520ee1624f76a6d22
feLycks <felix.obiero@student.moringaschool.com>
2022-03-03 23:39:26 +0300

    routing pages

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(req):
+    return render(req, 'index.html')
+
+def register(req):
+    return render(req,'register.html')
+
+def login(req):
+    return render(req, 'login.html')
+
+
